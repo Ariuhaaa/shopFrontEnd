d1b09ef1ec74ce1d57edb0392f2dd4c518dbcd8d
Morning <morning@class3-3s-iMac.local>
2023-02-16 12:48:34 +0800

    2.16

--- src/components/Admin/User.js
+++ src/components/Admin/User.js
@@ -1,143 +1,140 @@
-// import React, { useState } from "react";
-// import Form from "react-bootstrap/Form";
-// import Modal from "react-bootstrap/Modal";
-// import Button from "react-bootstrap/Button";
-// import { FormControl, FormLabel } from "react-bootstrap";
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import { FormControl, FormLabel } from "react-bootstrap";
 
-// export default function User() {
+export default function User({ show, setShow, onSave }) {
+  const init = {
+    firstname: "",
+    lastname: "",
+    username: "",
+    password: "",
+    email: "",
+  };
 
-//     const [show, setShow] = useState(searchParams && true);
+  const [userItem, setUserItem] = useState(init);
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [isValid, setIsValid] = useState([]);
 
-//     const init = {
-//       firstname: "",
-//       lastname: "",
-//       username: "",
-//       password: "",
-//       email: "",
-//     };
+  const onClose = () => {
+    // setSearchParams({});
+    setShow(false);
+  };
 
-//     const [userItem, setUserItem] = useState(init);
-//     const [confirmPassword, setConfirmPassword] = useState("");
-//     const [isValid, setIsValid] = useState([]);
+  return (
+    <Modal show={show} onHide={onClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Total</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Group className="mb-3">
+            <FormLabel>Овог</FormLabel>
+            <FormControl
+              value={userItem.lastname}
+              onChange={(e) => {
+                setUserItem({ ...userItem, lastname: e.target.value });
+              }}
+              type="text"
+            />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <FormLabel>Нэр</FormLabel>
+            <FormControl
+              value={userItem.firstname}
+              onChange={(e) => {
+                setUserItem({ ...userItem, firstname: e.target.value });
+              }}
+              type="text"
+            />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <FormLabel>Хэрэглэгчийн нэр</FormLabel>
+            <FormControl
+              value={userItem.username}
+              onChange={(e) => {
+                setUserItem({ ...userItem, username: e.target.value });
+              }}
+              type="text"
+            />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <FormLabel>Нууц үг</FormLabel>
+            <FormControl
+              value={userItem.password}
+              onChange={(e) => {
+                setUserItem({ ...userItem, password: e.target.value });
 
-//     const onClose = () => {
-//       setSearchParams({});
-//       setShow(false);
-//     };
-//   }
-//   return (
-//     <Modal show={show} onHide={onClose}>
-//       <Modal.Header closeButton>
-//         <Modal.Title>{title}</Modal.Title>
-//       </Modal.Header>
-//       <Modal.Body>
-//         <Form>
-//           <Form.Group className="mb-3">
-//             <FormLabel>Овог</FormLabel>
-//             <FormControl
-//               value={userItem.lastname}
-//               onChange={(e) => {
-//                 setUserItem({ ...userItem, lastname: e.target.value });
-//               }}
-//               type="text"
-//             />
-//           </Form.Group>
-//           <Form.Group className="mb-3">
-//             <FormLabel>Нэр</FormLabel>
-//             <FormControl
-//               value={userItem.firstname}
-//               onChange={(e) => {
-//                 setUserItem({ ...userItem, firstname: e.target.value });
-//               }}
-//               type="text"
-//             />
-//           </Form.Group>
-//           <Form.Group className="mb-3">
-//             <FormLabel>Хэрэглэгчийн нэр</FormLabel>
-//             <FormControl
-//               value={userItem.username}
-//               onChange={(e) => {
-//                 setUserItem({ ...userItem, username: e.target.value });
-//               }}
-//               type="text"
-//             />
-//           </Form.Group>
-//           <Form.Group className="mb-3">
-//             <FormLabel>Нууц үг</FormLabel>
-//             <FormControl
-//               value={userItem.password}
-//               onChange={(e) => {
-//                 setUserItem({ ...userItem, password: e.target.value });
+                const regex = new RegExp(/^(?=.*[a-z])(?=.*[A-Z])/g);
+                const regex1 = new RegExp(/(?=.*[!@#$%^&*])(?=.*[0-9])/g);
+                const regex2 = new RegExp(/(?=.{8,})/g);
 
-//                 const regex = new RegExp(/^(?=.*[a-z])(?=.*[A-Z])/g);
-//                 const regex1 = new RegExp(/(?=.*[!@#$%^&*])(?=.*[0-9])/g);
-//                 const regex2 = new RegExp(/(?=.{8,})/g);
+                const para = e.target.value;
 
-//                 const para = e.target.value;
+                console.log(regex.test(para));
 
-//                 console.log(regex.test(para));
+                const newArr = [];
+                newArr.push(
+                  regex.test(para),
+                  regex1.test(para),
+                  regex2.test(para)
+                );
 
-//                 const newArr = [];
-//                 newArr.push(
-//                   regex.test(para),
-//                   regex1.test(para),
-//                   regex2.test(para)
-//                 );
-
-//                 setIsValid(newArr);
-//               }}
-//               type="text"
-//             />
-//             <div>
-//               <ul>
-//                 <li style={{ color: isValid[0] ? "green" : "grey" }}>
-//                   Том жижиг үсэг агуулсан байх
-//                 </li>
-//                 <li style={{ color: isValid[1] ? "green" : "grey" }}>
-//                   Тоо болон тэмдэгтийг агуулсан байх
-//                 </li>
-//                 <li style={{ color: isValid[2] ? "green" : "grey" }}>
-//                   8-аас дээш тэмдэгтийг агуулсан байх
-//                 </li>
-//               </ul>
-//             </div>
-//           </Form.Group>
-//           <Form.Group className="mb-3">
-//             <FormLabel>Нууц үг давтах</FormLabel>
-//             <FormControl
-//               value={confirmPassword}
-//               onChange={(e) => {
-//                 setConfirmPassword(e.target.value);
-//               }}
-//               type="text"
-//             />
-//           </Form.Group>
-//           <Form.Group className="mb-3">
-//             <FormLabel>Email</FormLabel>
-//             <FormControl
-//               value={userItem.email}
-//               onChange={(e) => {
-//                 setUserItem({ ...userItem, email: e.target.value });
-//               }}
-//               type="email"
-//             />
-//           </Form.Group>
-//         </Form>
-//       </Modal.Body>
-//       <Modal.Footer>
-//         <Button variant="secondary" onClick={onClose}>
-//           Close
-//         </Button>
-//         <Button
-//           variant="primary"
-//           onClick={() => {
-//             onClose();
-//             onSave(userItem);
-//           }}
-//         >
-//           Save
-//         </Button>
-//       </Modal.Footer>
-//     </Modal>
-//   );
-// }
+                setIsValid(newArr);
+              }}
+              type="text"
+            />
+            <div>
+              <ul>
+                <li style={{ color: isValid[0] ? "green" : "grey" }}>
+                  Том жижиг үсэг агуулсан байх
+                </li>
+                <li style={{ color: isValid[1] ? "green" : "grey" }}>
+                  Тоо болон тэмдэгтийг агуулсан байх
+                </li>
+                <li style={{ color: isValid[2] ? "green" : "grey" }}>
+                  8-аас дээш тэмдэгтийг агуулсан байх
+                </li>
+              </ul>
+            </div>
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <FormLabel>Нууц үг давтах</FormLabel>
+            <FormControl
+              value={confirmPassword}
+              onChange={(e) => {
+                setConfirmPassword(e.target.value);
+              }}
+              type="text"
+            />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <FormLabel>Email</FormLabel>
+            <FormControl
+              value={userItem.email}
+              onChange={(e) => {
+                setUserItem({ ...userItem, email: e.target.value });
+              }}
+              type="email"
+            />
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={onClose}>
+          Close
+        </Button>
+        <Button
+          variant="primary"
+          onClick={() => {
+            onClose();
+            onSave(userItem);
+          }}
+        >
+          Save
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
--- src/components/Admin/UserList.js
+++ src/components/Admin/UserList.js
@@ -1,64 +1,83 @@
-// import React, { useEffect, useState } from "react";
-// // import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import User from "./User";
+// import { useNavigate } from "react-router-dom";
 
-// export default function UserList() {
-//   //   const navigate = useNavigate();
-//   const [user, setUser] = useState([]);
+export default function UserList() {
+  //   const navigate = useNavigate();
+  const [user, setUser] = useState([]);
+  const [show, setShow] = useState(false);
 
-//   function getData() {
-//     //GET
-//     fetch("http://localhost:8080/api/user")
-//       .then((res) => res.json())
-//       .then((data) => {
-//         setUser(data.result);
-//       });
-//   }
+  function getData() {
+    //GET
+    fetch("http://localhost:8080/api/user")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setUser(data.result);
+      });
+  }
 
-//   useEffect(() => {
-//     getData();
-//   }, []);
+  useEffect(() => {
+    getData();
+  }, []);
 
-//   return (
-//     <div className="container">
-//       <h5>User List</h5>
-//       <br />
-//       <button
-//         className="btn btn-primary"
-//         //   onClick={Modal}
-//       >
-//         {/* onClick={() => navigate("/UserList")} */}
-//         Add User
-//       </button>
-//       <br />
+  const onSave = (user) => {
+    console.log(user);
+    fetch("http://localhost:8080/api/user", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        getData();
+        // setUser(data.result);
+      });
+  };
 
-//       <br />
-//       <table className="table ">
-//         <thead>
-//           <tr>
-//             <th scope="col">#</th>
-//             <th scope="col">Username</th>
-//             <th scope="col">Id</th>
-//             <th scope="col">Email</th>
-//             <th scope="col">Action</th>
-//           </tr>
-//         </thead>
-//         <tbody>
-//           {/* <tr>
-//             <td>{index + 1}</td>
-//             <td>{e.username + e.lastname}</td>
-//             <td>{e.id}</td>
-//             <td>{e.email}</td>
-//             <td className="d-flex gap-2">
-//               <div className="btn btn-primary" onClick={() => e.id}>
-//                 Edit
-//               </div>
-//               <div className="btn btn-danger" onClick={() => e.id}>
-//                 Delete
-//               </div>
-//             </td>
-//           </tr> */}
-//         </tbody>
-//       </table>
-//     </div>
-//   );
-// }
+  return (
+    <div className="container">
+      <h5>User List</h5>
+      <br />
+      <button className="btn btn-primary" onClick={() => setShow(true)}>
+        {/* onClick={() => navigate("/UserList")} */}
+        Add User
+      </button>
+      <br />
+
+      <br />
+      <table className="table ">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Username</th>
+            <th scope="col">Id</th>
+            <th scope="col">Email</th>
+            <th scope="col">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {user.map((e, index) => {
+            return (
+              <tr>
+                <td>{index + 1}</td>
+                <td>{e.username + e.lastname}</td>
+                <td>{e.id}</td>
+                <td>{e.email}</td>
+                <td className="d-flex gap-2">
+                  <div className="btn btn-primary" onClick={() => e.id}>
+                    Edit
+                  </div>
+                  <div className="btn btn-danger" onClick={() => e.id}>
+                    Delete
+                  </div>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <User show={show} setShow={setShow} onSave={onSave} />
+    </div>
+  );
+}
